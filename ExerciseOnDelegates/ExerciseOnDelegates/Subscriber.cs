cf44b99dbd9a9dad7bf8d0ec75301e2f52d0f16a
Светлана Быкова <fffcthkbvsq@mail.ru>
2020-12-16 18:22:20 +0300

    init

--- ExerciseOnDelegates/ExerciseOnDelegates/Subscriber.cs
+++ ExerciseOnDelegates/ExerciseOnDelegates/Subscriber.cs
@@ -2,23 +2,29 @@
 using System.Collections.Generic;
 using System.Text;
 
-/*В классе Subscriber должно быть три метода :
+/*
 1. метод void (без параметров), который выводит на консоль: "Привет Инкогнито"
 2. метод void с текстовым параметров lastName, который выводит на консоль: "Привет " + lastName
 3. метод, который возвращает string "Моё Уважение!" и выводит на консоль "Здравствуйте Верховный Ситх!"*/
 
 namespace ExerciseOnDelegates
 {
-    class Subscriber
+    public class Subscriber
     {
-        public void dfg()
+        public void WithoutParametrs()
         {
             Console.WriteLine("Привет инкогнито");
         }
-        public void sdf(string lastName)
-        {
+        public void LastName(string lastName)
+        {            
             Console.WriteLine("Привет, " + lastName);
+            
         }
-        public string djphf ()
+        public void ReturnsString(string lastName)
+        {            
+            Console.WriteLine("Здравствуйте, Верховный Ситх");
+            Console.WriteLine("Моё уважение!");
+            return;
+        }
     }
 }
